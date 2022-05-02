37980591498c12888f4c4ff0672cbe04e180415b
Federico Krenn <fedekrenn@gmail.com>
2022-05-02 00:31:54 -0300

    Servicio y servidor de tareas cargado

--- src/app/components/tasks/tasks.component.ts
+++ src/app/components/tasks/tasks.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { TaskService } from 'src/app/service/task.service';
 import { Task } from 'src/app/tasks';
-import { TASKS } from 'src/app/mock-tasks';
 
 @Component({
   selector: 'app-tasks',
@@ -8,11 +8,17 @@ import { TASKS } from 'src/app/mock-tasks';
   styleUrls: ['./tasks.component.css']
 })
 export class TasksComponent implements OnInit {
-  tasks: Task[] = TASKS;
+  tasks: Task[] = [];
   
-  constructor() { }
+  constructor(
+    private taskService: TaskService
+  ) { }
 
   ngOnInit(): void {
+    // Es como la promesa
+    this.taskService.getTasks().subscribe((tasks) => {
+      this.tasks = tasks;
+    });
   }
 
 }
---
+++ src/app/service/task.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHandler} from '@angular/common/http';
+import { Observable, of} from 'rxjs';
+import { Task } from 'src/app/tasks';
+import { TASKS } from 'src/app/mock-tasks';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TaskService {
+  private apiUrl = 'http://localhost:5000/tasks'
+
+  constructor(
+    private http:HttpClient
+  ) { }
+
+  getTasks(): Observable<Task[]>{
+
+    return this.http.get<Task[]>(this.apiUrl)
+  }
+}
