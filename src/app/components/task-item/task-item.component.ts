f5805e4f80f3d1ac08787594e759db3a3ddb620a
Federico Krenn <fedekrenn@gmail.com>
2022-05-04 23:48:16 -0300

    Se logra acceso a la bd

--- src/app/components/task-item/task-item.component.ts
+++ src/app/components/task-item/task-item.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import { Task } from '../../tasks';
 import { TASKS } from 'src/app/mock-tasks';
 import { faTimes } from '@fortawesome/free-solid-svg-icons';
@@ -10,10 +10,21 @@ import { faTimes } from '@fortawesome/free-solid-svg-icons';
 })
 export class TaskItemComponent implements OnInit {
   @Input() task: Task = TASKS[0]
+  @Output() onDeleteTask: EventEmitter<Task> = new EventEmitter()
+  @Output() onToggleReminder: EventEmitter<Task> = new EventEmitter()
+  
   faTimes = faTimes;
+  
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  onDelete(task: Task){
+    this.onDeleteTask.emit(task);
+  }
+
+  onToggle(task: Task){
+    this.onToggleReminder.emit(task);
+  }
 }
--- src/app/service/task.service.ts
+++ src/app/service/task.service.ts
@@ -1,21 +1,39 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpHandler} from '@angular/common/http';
-import { Observable, of} from 'rxjs';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable, of } from 'rxjs';
 import { Task } from 'src/app/tasks';
 import { TASKS } from 'src/app/mock-tasks';
+
+const httpOptions = {
+  headers: new HttpHeaders({
+    'Content-Type':'application/json'
+  })
+}
 
 @Injectable({
   providedIn: 'root'
 })
+
 export class TaskService {
   private apiUrl = 'http://localhost:5000/tasks'
 
   constructor(
-    private http:HttpClient
+    private http: HttpClient
   ) { }
 
-  getTasks(): Observable<Task[]>{
-
+  getTasks(): Observable<Task[]> {
     return this.http.get<Task[]>(this.apiUrl)
   }
+
+  deleteTask(task:Task): Observable<Task>{
+    const url = `${this.apiUrl}/${task.id}`
+    return this.http.delete<Task>(url)
+  }
+
+  updateTaskReminder(task: Task): Observable<Task>{
+    const url = `${this.apiUrl}/${task.id}`
+    return this.http.put<Task>(url, task, httpOptions)
+  }
+
+
 }
